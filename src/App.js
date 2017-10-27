c395a6d36db1e2d63698d7a9b5fe8ebba576353f
John <jdpoole@gmail.com>
2017-10-26 21:15:21 -0600

    full example

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import './App.css'
 import WorldMap from './WorldMap'
 import BarChart from './BarChart'
+import StreamGraph from './StreamGraph'
 import Brush from './Brush'
-
+import StatLine from './StatLine'
 import worlddata from './world'
 import { range } from 'd3-array'
 import { scaleThreshold } from 'd3-scale'
@@ -22,8 +23,7 @@ appdata
 const colorScale = scaleThreshold().domain([5,10,20,30]).range(["#75739F", "#5EAFC6", "#41A368", "#93C464"])
 
 class App extends Component {
-
-constructor(props){
+  constructor(props){
     super(props)
     this.onResize = this.onResize.bind(this)
     this.onHover = this.onHover.bind(this)
@@ -33,37 +33,40 @@ constructor(props){
   }
 
   onResize() {
-      this.setState({ screenWidth: window.innerWidth, screenHeight: window.innerHeight - 120 })
-    }
+    this.setState({ screenWidth: window.innerWidth, screenHeight: window.innerHeight - 120 })
+  }
 
-    onHover(d) {
-      this.setState({ hover: d.id })
-    }
+  onHover(d) {
+    this.setState({ hover: d.id })
+  }
 
-    onBrush(d) {
-      this.setState({ brushExtent: d })
-    }
+  onBrush(d) {
+    this.setState({ brushExtent: d })
+  }
 
-    componentDidMount() {
-      window.addEventListener('resize', this.onResize, false)
-      this.onResize()
-    }
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false)
+    this.onResize()
+  }
 
-   render() {
-     const filteredAppdata = appdata
-        .filter((d,i) => d.launchday >= this.state.brushExtent[0] && d.launchday <= this.state.brushExtent[1])
-      return (
-        <div className="App">
-          <div className="App-header">
-            <h2>d3ia dashboard</h2>
-          </div>
-          <div>
-          <Brush changeBrush={this.onBrush} size={[this.state.screenWidth, 50]} />
-          <WorldMap hoverElement={this.state.hover} onHover={this.onHover} colorScale={colorScale} data={filteredAppdata} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]} />
-          <BarChart hoverElement={this.state.hover} onHover={this.onHover} colorScale={colorScale} data={filteredAppdata} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]} />
-          </div>
+  render() {
+    const filteredAppdata = appdata
+      .filter((d,i) => d.launchday >= this.state.brushExtent[0] && d.launchday <= this.state.brushExtent[1])
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>d3ia dashboard</h2>
         </div>
-      )
-   }
+        <div>
+        <StatLine allData={appdata} filteredData={filteredAppdata} />
+        <StreamGraph hoverElement={this.state.hover} onHover={this.onHover} colorScale={colorScale} data={filteredAppdata} size={[this.state.screenWidth, this.state.screenHeight / 2]} />
+        <Brush changeBrush={this.onBrush} size={[this.state.screenWidth, 50]} />
+        <WorldMap hoverElement={this.state.hover} onHover={this.onHover} colorScale={colorScale} data={filteredAppdata} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]} />
+        <BarChart hoverElement={this.state.hover} onHover={this.onHover} colorScale={colorScale} data={filteredAppdata} size={[this.state.screenWidth / 2, this.state.screenHeight / 2]} />
+        </div>
+      </div>
+    )
+  }
 }
+
 export default App
