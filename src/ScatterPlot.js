1392d2baa3f2c0cb2c5487ea82fc48815a8b8db5
john <jdpoole@gmail.com>
2017-11-26 10:07:34 -0700

    tick format

--- src/ScatterPlot.js
+++ src/ScatterPlot.js
@@ -24,51 +24,48 @@ class ScatterPlot extends Component {
   createScatterPlot() {
     const node = this.node
 
-    var formatCount = d3.format(",.0f");
-
     var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = this.props.size[0] - margin.left - margin.right,
-        height = this.props.size[1] - margin.top - margin.bottom;
+    width = this.props.size[0] - margin.left - margin.right,
+    height = this.props.size[1] - margin.top - margin.bottom;
 
 
     var x = d3.scaleLinear()
-    .rangeRound([0, width]);
-    var y = d3.scaleLinear().range([height, 0]);
+    .rangeRound([margin.left, width]);
+    var y = d3.scaleLinear().range([height, margin.top]);
 
     d3.select(node)
-      .selectAll(".dot").remove();
+    .selectAll(".dot").remove();
 
     if(  typeof this.props.data === 'undefined' || this.props.data.length === 0 ||  this.props.data[0].length === 0 )
-      return;
-      var data = this.props.data[0];
+    return;
+    var data = this.props.data[0];
 
     x.domain(d3.extent(data, function(d) {
       return d[2].count;
-     }));
-    y.domain([0, d3.max(data, function(d) {
-      return d[2].treated;
-    })]);
+    }));
+    y.domain([0, 7]);
+    // d3.max(data, function(d) {
+    //    return d[2].treated;
+    //    })]);
 
     d3.select(node).append("g")
     .attr("transform", "translate(" + margin.left + ",0)")
-.selectAll("dot")
-         .data(data)
-       .enter().append("circle")
-         .attr("r", 5)
-         .attr("cx", function(d) {
-           return x(d[2].count); })
-         .attr("cy", function(d) {
-           return y(d[2].treated); });
+    .selectAll("dot")
+    .data(data)
+    .enter().append("circle")
+    .attr("r", 5)
+    .attr("cx", function(d) {  return x(d[2].count); })
+    .attr("cy", function(d) {return y(d[2].treated); });
 
-           // Add the X Axis
-             d3.select(node).append("g")
-                 .attr("transform", "translate("+margin.left+"," + height + ")")
-                 .call(d3.axisBottom(x));
+    // Add the X Axis
+    d3.select(node).append("g")
+    .attr("transform", "translate("+margin.left+"," + height + ")")
+    .call(d3.axisBottom(x) .ticks(5) .tickFormat(d3.format(",d")));
 
-             // Add the Y Axis
-             d3.select(node).append("g")
-             .attr("transform", "translate(" + margin.left + ",0)")
-                 .call(d3.axisLeft(y));
+    // Add the Y Axis
+    d3.select(node).append("g")
+    .attr("transform", "translate(" + margin.left + ",0)")
+    .call(d3.axisLeft(y));
   }
 
   render() {
